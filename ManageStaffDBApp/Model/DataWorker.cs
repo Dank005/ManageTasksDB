3aedea4898a7008500d17dae596872ef577674d9
Dank005 <daniil.yushkov2000@mail.ru>
2021-08-31 14:11:42 +0500

    first commit

---
+++ ManageStaffDBApp/Model/DataWorker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageStaffDBApp.Model
+{
+    static class DataWorker
+    {
+        // creating fields
+        // editing
+        // deleting
+    }
+}
